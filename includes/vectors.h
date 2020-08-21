dfb9106a9a59893593d863bfd7f725fc731cbd76
ltammie <55656332+ltammie@users.noreply.github.com>
2020-08-21 15:40:13 +0300

    kernel rendering

--- includes/vectors.h
+++ includes/vectors.h
@@ -1,11 +1,12 @@
 #ifndef VECTORS_H
 #define VECTORS_H
 
-typedef struct			s_vector
+typedef struct			s_vector3
 {
 	float x;
 	float y;
 	float z;
-}						t_vector;
+}						t_vector3;
 
+t_vector3				new_vector3(int x, int y, int z);
 #endif
---
+++ sources/vectors.c
@@ -0,0 +1,11 @@
+#include "../includes/vectors.h"
+
+t_vector3		new_vector3(int x, int y, int z)
+{
+	t_vector3 res;
+
+	res.x = x;
+	res.y = y;
+	res.z = x;
+	return (res);
+}
