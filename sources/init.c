df35b0ea38e8bdc3fc13b33ba20cf5d2e8f1c675
ltammie <55656332+ltammie@users.noreply.github.com>
2020-09-03 18:33:51 +0300

    da hell is that error

--- sources/init.c
+++ sources/init.c
@@ -19,13 +19,16 @@ t_rt				*init_data()
 	ft_printf("width = %d, height = %d\n", data->width, data->height);
 	data->renderer = SDL_CreateRenderer(data->window, -1, SDL_RENDERER_ACCELERATED);
 	!(data->renderer) ? error(RENDER_INIT_ERROR, SDL_GetError()) : 0;
+	ft_printf("render_good\n");
 	data->texture = SDL_CreateTexture(data->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, data->width, data->height);
 	!(data->texture) ? error(TEXTURE_LOAD_ERROR, SDL_GetError()) : 0;
+	ft_printf("texture_good\n");
 	data->cl_path = return_path();
+	ft_printf("path_good\n");
 	data->o[0] = new_sphere((cl_float3){10.0f, 0.0f, -15.0f}, 2.0f, new_material((cl_float4){255, 0, 0, 0}));
 	data->o[1] = new_sphere((cl_float3){0.0f, 0.0f, -10.0f}, 1.0f, new_material((cl_float4){0, 255, 0, 0}));
 	data->o[2] = new_sphere((cl_float3){-20.0f, 0.0f, -15.0f}, 4.0f, new_material((cl_float4){0, 0, 255, 0}));
-
+	ft_printf("objects_good\n");
 	return (data);
 }
 
--- sources/main.c
+++ sources/main.c
@@ -9,26 +9,25 @@ int main(int argc, char **argv)
 
 	data = init_data();
 	ft_printf("init\n");
-	data->cl->kernel_source = get_kernel_source(data->cl, data->cl_path);
-	ft_printf("kernel_source\n");
-
-//	cl_init(data->cl, data->width, data->height);
-//	quit = 0;
-//	while (quit != 1)
-//	{
-//		SDL_RenderClear(data->renderer);
-//		res = render(data->cl, data->width, data->height, data->o);
-//		update_texture(data->texture, data->width, data->height, res);
-//		SDL_RenderCopy(data->renderer, data->texture, NULL, NULL);
-//		SDL_RenderPresent(data->renderer);
-//		while (SDL_PollEvent(&e))
-//		{
-//			if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE))
-//				quit = 1;
-//			else
-//				controller(&e, data);
-//		}
-//	}
+//	data->cl->kernel_source = get_kernel_source(data->cl, data->cl_path);
+//	ft_printf("kernel_source\n");
+	cl_init(data->cl, data->width, data->height);
+	quit = 0;
+	while (quit != 1)
+	{
+		SDL_RenderClear(data->renderer);
+		res = render(data->cl, data->width, data->height, data->o);
+		update_texture(data->texture, data->width, data->height, res);
+		SDL_RenderCopy(data->renderer, data->texture, NULL, NULL);
+		SDL_RenderPresent(data->renderer);
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE))
+				quit = 1;
+			else
+				controller(&e, data);
+		}
+	}
 	close_rt(data);
 	return (0);
 }
