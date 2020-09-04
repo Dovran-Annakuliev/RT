8392ddb4b00d5ab4ba425db963b00885ad19a393
ltammie <55656332+ltammie@users.noreply.github.com>
2020-09-04 18:27:22 +0300

    new canvas to viewport conversion

--- sources/controllers/keyboard_controller.c
+++ sources/controllers/keyboard_controller.c
@@ -4,12 +4,12 @@ void 		keyboard_controller(SDL_Event *e, t_rt *data)
 {
 	if (e->type == SDL_KEYDOWN)
 	{
-		e->key.keysym.sym == SDLK_a ? data->o[1].s_center.x-= 0.1 : 0;
-		e->key.keysym.sym == SDLK_d ? data->o[1].s_center.x+= 0.1 : 0;
-		e->key.keysym.sym == SDLK_w ? data->o[1].s_center.y-= 0.1 : 0;
-		e->key.keysym.sym == SDLK_s ? data->o[1].s_center.y+= 0.1 : 0;
-		e->key.keysym.sym == SDLK_r ? data->o[1].s_center.z+= 0.1 : 0;
-		e->key.keysym.sym == SDLK_f ? data->o[1].s_center.z-= 0.1 : 0;
+		e->key.keysym.sym == SDLK_a ? data->o[1].s_center.x-= 0.2 : 0;
+		e->key.keysym.sym == SDLK_d ? data->o[1].s_center.x+= 0.2 : 0;
+		e->key.keysym.sym == SDLK_w ? data->o[1].s_center.y-= 0.2 : 0;
+		e->key.keysym.sym == SDLK_s ? data->o[1].s_center.y+= 0.2 : 0;
+		e->key.keysym.sym == SDLK_r ? data->o[1].s_center.z+= 0.2 : 0;
+		e->key.keysym.sym == SDLK_f ? data->o[1].s_center.z-= 0.2 : 0;
 	}
 
 }
