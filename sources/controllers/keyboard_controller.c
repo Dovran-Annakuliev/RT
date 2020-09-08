b7374f02d9b04da000e709ae82f353fbbe2640b3
ltammie <55656332+ltammie@users.noreply.github.com>
2020-09-08 12:44:18 +0300

    changed anit-aliasing samples compute to be the power of 2

--- sources/controllers/keyboard_controller.c
+++ sources/controllers/keyboard_controller.c
@@ -13,8 +13,7 @@ void 		keyboard_controller(SDL_Event *e, t_rt *data)
 		e->key.keysym.sym == SDLK_f ? data->camera.look_from.z -= 0.2 : 0;
 		if (e->key.keysym.sym == SDLK_p)
 		{
-			data->samples += (data->samples == 1 ? 1 : 2);
-			data->samples %= 17;
+			data->samples = data->samples % 16 == 0 ? 1 : data->samples * 2;
 			free(data->randoms);
 			data->randoms = new_random_array(data->width, data->height, data->samples);
 			printf("samples = %d\n", data->samples);
